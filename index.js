207b1baf1c0a3a732fbb771a002a406046b2adce
Elijah wines <elijahwines84@yahoo.com>
2020-08-11 14:42:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
   let load = document.getElementById('text');
   load.texContent('This is really cool!')
 });
+
+const text = document.getElementById('text');
+text.addEventListener('DOMContentLoaded', function(e) {
+  text.textContent = "This is really cool!"
+})
