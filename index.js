29daedc3b66cf5885f7b750ab3e17a4b35d97a6b
Elijah wines <elijahwines84@yahoo.com>
2020-08-11 14:48:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
   text = document.getElementById('text')
-  text.innerHtml = "This is really cool!";
+  text.textContent = "This is really cool!";
 });
