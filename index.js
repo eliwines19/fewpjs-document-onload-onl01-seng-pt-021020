2b48c12a3aaa0432e2e541b988dafdb9914f35cf
Elijah wines <elijahwines84@yahoo.com>
2020-08-11 14:43:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 // Your code goes here
-document.addEventListener("DOMContentLoaded", function() {
-  let load = document.getElementById('text');
-  load.texContent('This is really cool!')
-});
-
 const text = document.getElementById('text');
 text.addEventListener('DOMContentLoaded', function(e) {
   text.textContent = "This is really cool!";
